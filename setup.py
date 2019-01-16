797380294f3507e26cfcab4ee92a223949dcb462
Sridhar <adhikarla2sridhar@yahoo.com>
2019-01-16 21:22:41 +0100

    Final dist uploaded on to PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Basic_NN",
-    version="0.0.1",
+    version="0.0.2",
     author="Sridhar Adhikarla",
     author_email="adhikarla2sridhar@yahoo.com",
     description="A simple package to help you learn to implement NN",
